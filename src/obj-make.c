251e5421371a09acc79fb7be7ff3ab4904d9d38d
Nick McConnell <nckmccnnll@gmail.com>
2011-12-23 18:10:00 +1100

    Tidy up cave_clean_bold

--- src/obj-make.c
+++ src/obj-make.c
@@ -1909,7 +1909,7 @@ void place_object(int y, int x, bool good, bool great, bool exact_kind)
     if (!in_bounds(y, x))
 	return;
 
-    /* Hack -- require clean floor space */
+    /* Require clean floor space */
     if (!cave_clean_bold(y, x))
 	return;
 
