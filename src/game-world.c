8958e0e33072fcf8f451bcfbe8910d7fb8d713eb
Nick McConnell <nckmccnnll@gmail.com>
2021-09-06 07:33:18 +1000

    Add region.txt and parse it

--- src/game-world.c
+++ src/game-world.c
@@ -51,6 +51,8 @@ bool character_generated;	/* The character exists */
 bool character_dungeon;		/* The character has a dungeon */
 struct level *maps;
 struct level *world;
+struct world_region *region_info;
+char **region_terrain;
 struct gen_loc *gen_loc_list;	/* List of generated locations */
 u32b gen_loc_max = GEN_LOC_INCR;/* Maximum number of generated locations */
 u32b gen_loc_cnt;				/* Current number of generated locations */
--- src/game-world.h
+++ src/game-world.h
@@ -28,13 +28,18 @@ enum {
 	HEALTH_WOUNDED,
 	HEALTH_SOMEWHAT_WOUNDED,
 	HEALTH_UNHURT
+};
 
 /**
  * Size increment of the generated locations array
  */
 #define GEN_LOC_INCR 128
 
-};
+/**
+ * Maximum x and y values for region grids 
+ */
+#define MAX_Y_REGION 588
+#define MAX_X_REGION 735
 
 struct level {
 	int depth;
@@ -46,16 +51,35 @@ struct level {
 
 #define SMELL_STRENGTH 80
 
-extern uint16_t daycount;
-extern uint32_t seed_randart;
-extern uint32_t seed_flavor;
-extern int32_t turn;
+/**
+ * Information about regions
+ */
+struct world_region {
+    struct world_region *next;
+    unsigned int index;
+    char *name;
+    char *message;
+    char *text;
+
+    byte danger;       	           /**< Region danger */
+    u16b height;       	           /**< Region height */
+    u16b width;       	           /**< Region width */
+    u16b y_offset;                 /**< Region y location */
+    u16b x_offset;                 /**< Region x location */
+
+};
+
+extern u16b daycount;
+extern u32b seed_randart;
+extern u32b seed_flavor;
+extern s32b turn;
 extern bool character_generated;
 extern bool character_dungeon;
-
 extern const byte extract_energy[8];
 extern struct level *maps;
 extern struct level *world;
+extern struct world_region *region_info;
+extern char **region_terrain;
 extern struct gen_loc *gen_loc_list;
 extern u32b gen_loc_max;
 extern u32b gen_loc_cnt;
