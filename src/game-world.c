1757ed9c06f8c4055f3b68d89482c2e866d084e4
Nick McConnell <nckmccnnll@gmail.com>
2021-09-06 12:11:16 +1000

    Add landmark.txt and parse it

--- src/game-world.c
+++ src/game-world.c
@@ -53,6 +53,7 @@ struct level *maps;
 struct level *world;
 struct world_region *region_info;
 char **region_terrain;
+struct landmark *landmark_info;
 struct gen_loc *gen_loc_list;	/* List of generated locations */
 u32b gen_loc_max = GEN_LOC_INCR;/* Maximum number of generated locations */
 u32b gen_loc_cnt;				/* Current number of generated locations */
