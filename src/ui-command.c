2a529850fcbd1c578394575920d46b28c2eb7fe5
backwardsEric <55062581+backwardsEric@users.noreply.github.com>
2021-11-09 12:38:40 -0800

    Add Term_redraw_all() to redraw all terminals.  Use it to replace … (#5135)
    * Add Term_redraw_all() to redraw all terminals.  Use it to replace analogous code in do_cmd_redraw() and the SDL2 front end.  Call it after reloading preferences in ui-init.c and ui-display.c to resolve https://github.com/angband/angband/issues/5131 .
    
    * Also use Term_redraw_all() (rather than Term_redraw()) after loading colors.

--- src/ui-command.c
+++ src/ui-command.c
@@ -62,10 +62,6 @@
  */
 void do_cmd_redraw(void)
 {
-	int j;
-
-	term *old = Term;
-
 	/* Low level flush */
 	Term_flush();
 
@@ -116,14 +112,7 @@ void do_cmd_redraw(void)
 	}
 
 	/* Redraw every window */
-	for (j = 0; j < ANGBAND_TERM_MAX; j++) {
-		if (!angband_term[j]) continue;
-
-		Term_activate(angband_term[j]);
-		Term_redraw();
-		Term_fresh();
-		Term_activate(old);
-	}
+	(void) Term_redraw_all();
 }
 
 
