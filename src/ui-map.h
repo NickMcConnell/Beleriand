41f274058721bb2675519482afca46bb25d1c7d2
Nick McConnell <nckmccnnll@gmail.com>
2021-11-13 08:50:01 +1100

    Basic zooming, not implemented in-game, map expansion and proper movement still not done

--- src/ui-map.h
+++ src/ui-map.h
@@ -21,6 +21,7 @@ extern void grid_data_as_text(struct grid_data *g, int *ap, wchar_t *cp,
 extern void move_cursor_relative(int y, int x);
 extern void print_rel(wchar_t c, uint8_t a, int y, int x);
 extern void prt_map(void);
+extern void prt_map_zoomed(void);
 extern void display_map(int *cy, int *cx);
 extern void do_cmd_view_map(void);
 extern void mini_screenshot(game_event_type type, game_event_data *data,
