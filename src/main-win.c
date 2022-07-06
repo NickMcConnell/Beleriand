8be71ea5be7ae11574202fbeda1d1b7df678bc6e
Eric Branlund <ebranlund@fastmail.com>
2022-07-06 14:52:26 -0600

    For including the Windows sound module, test for SOUND_SDL or SOUND_SDL2 rather than USE_SDL or USE_SDL2 since the latter are what are currently set when using SDL sound. In the Windows and Mac front ends, match up the preprocessor defines guarding init_sound() to what's in sound-core.c and allow, at least in principle, the use of SDL sound with those front ends.

--- src/main-win.c
+++ src/main-win.c
@@ -83,7 +83,7 @@
 
 #define uint unsigned int
 
-#if (defined(WINDOWS) && !defined(USE_SDL)) && !defined(USE_SDL2)
+#if defined(WINDOWS) && !defined(USE_SDL) && !defined(USE_SDL2)
 
 #include "sound.h"
 #include "snd-win.h"
@@ -1185,7 +1185,7 @@ static bool init_graphics(void)
 	return (can_use_graphics);
 }
 
-#ifdef SOUND
+#if defined(SOUND) && !defined(SOUND_SDL) && !defined(SOUND_SDL2)
 
 /* Supported file types */
 enum {
@@ -1356,7 +1356,7 @@ errr init_sound_win(struct sound_hooks *hooks, int argc, char **argv)
 	/* Success */
 	return (0);
 }
-#endif /* SOUND */
+#endif /* SOUND && !SOUND_SDL && !SOUND_SDL2 */
 
 
 /**
@@ -5125,8 +5125,14 @@ static void init_stuff(void)
  */
 static void win_reinit(void)
 {
-	/* Initialise sound. */
+/* Initialise sound. */
+#ifdef SOUND
+#if defined(SOUND_SDL) || defined(SOUND_SOUND_SDL2)
+	init_sound("sdl", 0, NULL);
+#else
 	init_sound("win", 0, NULL);
+#endif /* else SOUND_SDL || SOUND_SDL2 */
+#endif /* SOUND */
 
 	/*
 	 * Watch for these events to set up and tear down protection against
