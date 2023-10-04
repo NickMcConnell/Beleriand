50e661390047fa0f54dca1fe1778baac59873604
Eric Branlund <ebranlund@fastmail.com>
2023-10-04 14:58:25 -0600

    For spoilers, use full set of flags rather than those currently in lore
    Resolves https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=781748 .

--- src/ui-mon-lore.c
+++ src/ui-mon-lore.c
@@ -101,12 +101,12 @@ void lore_description(textblock *tb, const struct monster_race *race,
 	/* Hack -- create a copy of the monster-memory that we can modify */
 	memcpy(lore, original_lore, sizeof(struct monster_lore));
 
-	/* Now get the known monster flags */
-	monster_flags_known(race, lore, known_flags);
-
 	/* Spoilers -- know everything */
 	if (spoilers || player_active_ability(player, "Lore-Master"))
 		cheat_monster_lore(race, lore);
+
+	/* Now get the known monster flags */
+	monster_flags_known(race, lore, known_flags);
 
 	/* Appending the title here simplifies code in the callers. It also causes
 	 * a crash when generating spoilers (we don't need titles for them anwyay)*/
