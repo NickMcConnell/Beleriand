angband/angband

commit 3126eb4f289e4b6e6f1d714f72cec048d6c9254d

Allow more menus to use the rogue-like movement keys for navigation. … (#5316)
* Allow more menus to use the rogue-like movement keys for navigation.  Related to https://github.com/angband/angband/issues/5298 .

* Adjust end-to-end test for reindexing of birth screen race choices.

src/mon-blows.c
@@ -227,8 +227,9 @@ static void steal_player_item(melee_effect_handler_context_t *context)
 				(split ? "one of your" : "your"), o_name);
 		} else {
 			/* Message */
-			msg("%s %s (%c) was stolen!", (split ? "One of your" : "Your"),
-				o_name, I2A(index));
+			msg("%s %s (%c) was stolen!",
+				(split ? "One of your" : "Your"), o_name,
+				gear_to_label(context->p, obj));
 
 			/* Steal and carry */
 			stolen = gear_object_for_use(context->p, obj, 1,
@@ -805,12 +806,13 @@ static void melee_effect_handler_EAT_FOOD(melee_effect_handler_context_t *contex
 		if (obj->number == 1) {
 			object_desc(o_name, sizeof(o_name), obj, ODESC_BASE,
 				context->p);
-			msg("Your %s (%c) was eaten!", o_name, I2A(index));
+			msg("Your %s (%c) was eaten!", o_name,
+				gear_to_label(context->p, obj));
 		} else {
 			object_desc(o_name, sizeof(o_name), obj,
 				ODESC_PREFIX | ODESC_BASE, context->p);
 			msg("One of your %s (%c) was eaten!", o_name,
-				I2A(index));
+				gear_to_label(context->p, obj));
 		}
 
 		/* Steal and eat */

src/obj-gear.c
@@ -442,11 +442,14 @@ bool minus_ac(struct player *p)
  */
 char gear_to_label(struct player *p, struct object *obj)
 {
+	/* Skip rogue-like cardinal direction movement keys. */
+	const char labels[] =
+		 "abcdefgimnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int i;
 
 	/* Equipment is easy */
 	if (object_is_equipped(p->body, obj)) {
-		return I2A(equipped_item_slot(p->body, obj));
+		return labels[equipped_item_slot(p->body, obj)];
 	}
 
 	/* Check the quiver */
@@ -459,7 +462,7 @@ char gear_to_label(struct player *p, struct object *obj)
 	/* Check the inventory */
 	for (i = 0; i < z_info->pack_size; i++) {
 		if (p->upkeep->inven[i] == obj) {
-			return I2A(i);
+			return labels[i];
 		}
 	}
 
@@ -991,7 +994,7 @@ void inven_wield(struct object *obj, int slot)
 		ODESC_PREFIX | ODESC_FULL, player);
 
 	/* Message */
-	msgt(MSG_WIELD, fmt, o_name, I2A(slot));
+	msgt(MSG_WIELD, fmt, o_name, gear_to_label(player, wielded));
 
 	/* Sticky flag geats a special mention */
 	if (of_has(wielded->flags, OF_STICKY)) {

src/ui-birth.c
@@ -501,9 +501,10 @@ static void init_birth_menu(struct menu *menu, int n_choices,
 	/* Initialise a basic menu */
 	menu_init(menu, MN_SKIN_SCROLL, &birth_iter);
 
-	/* A couple of behavioural flags - we want selections letters in
-	   lower case and a double tap to act as a selection. */
-	menu->selections = lower_case;
+	/* A couple of behavioural flags - we want selections as letters
+	   skipping the rogue-like cardinal direction movements and a
+	   double tap to act as a selection. */
+	menu->selections = all_letters_nohjkl;
 	menu->flags = MN_DBL_TAP;
 
 	/* Copy across the game's suggested initial selection, etc. */

src/ui-effect.c
@@ -40,7 +40,7 @@ static struct menu *effect_menu_new(struct effect *effect, int count,
 	char **ms;
 	char buf[80];
 
-	m->selections = lower_case;
+	m->selections = all_letters_nohjkl;
 
 	/* Collect a string for each effect. */
 	if (count > 0) {

src/ui-history.c
@@ -96,12 +96,14 @@ void history_display(void)
 				break;
 			}
 
+			case 'j':
 			case ARROW_DOWN: {
 				size_t scroll_to = first_item + 1;
 				first_item = (scroll_to < max_item ? scroll_to : max_item);
 				break;
 			}
 
+			case 'k':
 			case ARROW_UP: {
 				int scroll_to = first_item - 1;
 				first_item = (scroll_to >= 0 ? scroll_to : 0);

src/ui-knowledge.c
@@ -772,16 +772,11 @@ static void display_knowledge(const char *title, int *obj_list, int o_count,
 	int wid, hgt;
 	int i;
 	int prev_g = -1;
-
-	int omode = OPT(player, rogue_like_commands);
 	ui_event ke;
 
 	/* Get size */
 	Term_get_size(&wid, &hgt);
 	browser_rows = hgt - 8;
-
-	/* Disable the roguelike commands for the duration */
-	OPT(player, rogue_like_commands) = false;
 
 	/* Determine if using tiles or not */
 	if (tiles) tiles = (current_graphics_mode->grafID != 0);
@@ -1059,9 +1054,6 @@ static void display_knowledge(const char *title, int *obj_list, int o_count,
 			redraw = true;
 		}
 	}
-
-	/* Restore roguelike option */
-	OPT(player, rogue_like_commands) = omode;
 
 	/* Prompt */
 	if (!grp_cnt)
@@ -2326,10 +2318,10 @@ static const char *feat_prompt(int oid)
 {
 	(void)oid;
 		switch (f_uik_lighting) {
-				case LIGHTING_LIT:  return ", 'l/L' for lighting (lit)";
-                case LIGHTING_TORCH: return ", 'l/L' for lighting (torch)";
-				case LIGHTING_LOS:  return ", 'l/L' for lighting (LOS)";
-				default:	return ", 'l/L' for lighting (dark)";
+				case LIGHTING_LIT:  return ", 't/T' for lighting (lit)";
+                case LIGHTING_TORCH: return ", 't/T' for lighting (torch)";
+				case LIGHTING_LOS:  return ", 't/T' for lighting (LOS)";
+				default:	return ", 't/T' for lighting (dark)";
 		}		
 }
 
@@ -2339,14 +2331,14 @@ static const char *feat_prompt(int oid)
 static void f_xtra_act(struct keypress ch, int oid)
 {
 	/* XXX must be a better way to cycle this */
-	if (ch.code == 'l') {
+	if (ch.code == 't') {
 		switch (f_uik_lighting) {
 				case LIGHTING_LIT:  f_uik_lighting = LIGHTING_TORCH; break;
                 case LIGHTING_TORCH: f_uik_lighting = LIGHTING_LOS; break;
 				case LIGHTING_LOS:  f_uik_lighting = LIGHTING_DARK; break;
 				default:	f_uik_lighting = LIGHTING_LIT; break;
 		}		
-	} else if (ch.code == 'L') {
+	} else if (ch.code == 'T') {
 		switch (f_uik_lighting) {
 				case LIGHTING_DARK:  f_uik_lighting = LIGHTING_LOS; break;
                 case LIGHTING_LOS: f_uik_lighting = LIGHTING_TORCH; break;
@@ -2511,7 +2503,7 @@ static void trap_lore(int oid)
 static const char *trap_prompt(int oid)
 {
 	(void)oid;
-	return ", 'l' to cycle lighting";
+	return ", 't' to cycle lighting";
 }
 
 /**
@@ -2520,14 +2512,14 @@ static const char *trap_prompt(int oid)
 static void t_xtra_act(struct keypress ch, int oid)
 {
 	/* XXX must be a better way to cycle this */
-	if (ch.code == 'l') {
+	if (ch.code == 't') {
 		switch (t_uik_lighting) {
 				case LIGHTING_LIT:  t_uik_lighting = LIGHTING_TORCH; break;
                 case LIGHTING_TORCH: t_uik_lighting = LIGHTING_LOS; break;
 				case LIGHTING_LOS:  t_uik_lighting = LIGHTING_DARK; break;
 				default:	t_uik_lighting = LIGHTING_LIT; break;
 		}		
-	} else if (ch.code == 'L') {
+	} else if (ch.code == 'T') {
 		switch (t_uik_lighting) {
 				case LIGHTING_DARK:  t_uik_lighting = LIGHTING_LOS; break;
                 case LIGHTING_LOS: t_uik_lighting = LIGHTING_TORCH; break;
@@ -3049,7 +3041,6 @@ static void do_cmd_knowledge_shapechange(const char *name, int row)
 	struct menu* m;
 	struct player_shape **sarray;
 	const char **narray;
-	int omode;
 	int h, mark, mark_old;
 	bool displaying, redraw;
 	struct player_shape *s;
@@ -3087,10 +3078,6 @@ static void do_cmd_knowledge_shapechange(const char *name, int row)
 
 	screen_save();
 	clear_from(0);
-
-	/* Disable the roguelike commands for the duration */
-	omode = OPT(player, rogue_like_commands);
-	OPT(player, rogue_like_commands) = false;
 
 	h = 0;
 	mark = 0;
@@ -3169,9 +3156,6 @@ static void do_cmd_knowledge_shapechange(const char *name, int row)
 			shape_lore(sarray[mark]);
 		}
 	}
-
-	/* Restore roguelike option */
-	OPT(player, rogue_like_commands) = omode;
 
 	screen_load();
 
@@ -3266,7 +3250,7 @@ void textui_knowledge_init(void)
 	menu_setpriv(menu, N_ELEMENTS(knowledge_actions), knowledge_actions);
 
 	menu->title = "Display current knowledge";
-	menu->selections = lower_case;
+	menu->selections = all_letters_nohjkl;
 	/* Shortcuts to get the contents of the stores by number; does prevent
 	 * the normal use of 4 and 6 to go to the previous or next menu */
 	menu->cmd_keys = "12345678";
@@ -3498,21 +3482,25 @@ void do_cmd_messages(void)
 
 				case ARROW_LEFT:
 				case '4':
+				case 'h':
 					q = (q >= wid / 2) ? (q - wid / 2) : 0;
 					break;
 
 				case ARROW_RIGHT:
 				case '6':
+				case 'l':
 					q = q + wid / 2;
 					break;
 
 				case ARROW_UP:
 				case '8':
+				case 'k':
 					if (i + 1 < n) i += 1;
 					break;
 
 				case ARROW_DOWN:
 				case '2':
+				case 'j':
 				case KC_ENTER:
 					i = (i >= 1) ? (i - 1) : 0;
 					break;

src/ui-menu.c
@@ -38,6 +38,7 @@ const uint8_t curs_attrs[2][2] =
 const char lower_case[] = "abcdefghijklmnopqrstuvwxyz";
 const char upper_case[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const char all_letters[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const char all_letters_nohjkl[] = "abcdefgimnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 /**
  * Forward declarations

src/ui-menu.h
@@ -55,6 +55,7 @@ extern const uint8_t curs_attrs[2][2];
 extern const char lower_case[];			/* abc..z */
 extern const char upper_case[];			/* ABC..Z */
 extern const char all_letters[];		/* abc..zABC..Z */
+extern const char all_letters_nohjkl[];		/* abc..gim..zABC..Z */
 
 
 /*

NickMcConnell/Beleriand

commit de5a6642c7306736303c6e87803af788704f3bb2

Port Vanilla Angband's changes for more menus navigable with the rogue-like movement keys (#511)
* Allow more menus to use the rogue-like movement keys for navigation. … (#5316)

* Allow more menus to use the rogue-like movement keys for navigation.  Related to https://github.com/angband/angband/issues/5298 .

* Adjust end-to-end test for reindexing of birth screen race choices.

* Use cleaner implementation of selectors for the item ignoring menu. Fixes regression introduced by https://github.com/angband/angband/commit/3126eb4f289e4b6e6f1d714f72cec048d6c9254d . Thanks to PowerWyrm for the report, http://angband.oook.cz/forum/showpost.php?p=158637&postcount=124 .

src/mon-blows.c
@@ -219,8 +219,9 @@ static void steal_player_item(melee_effect_handler_context_t *context)
 			split = true;
 
 		/* Message */
-		msg("%s %s (%c) was stolen!", (split ? "One of your" : "Your"), o_name,
-			I2A(index));
+		msg("%s %s (%c) was stolen!",
+			(split ? "One of your" : "Your"), o_name,
+			gear_to_label(context->p, obj));
 
 		/* Steal and carry */
 		stolen = gear_object_for_use(context->p, obj, 1,

src/obj-gear.c
@@ -415,17 +415,20 @@ bool minus_ac(struct player *p)
  */
 char gear_to_label(struct player *p, struct object *obj)
 {
+	/* Skip rogue-like cardinal direction movement keys. */
+	const char labels[] =
+		 "abcdefgimnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int i;
 
 	/* Equipment is easy */
 	if (object_is_equipped(p->body, obj) || object_is_in_quiver(p, obj)) {
-		return I2A(equipped_item_slot(p->body, obj));
+		return labels[equipped_item_slot(p->body, obj)];
 	}
 
 	/* Check the inventory */
 	for (i = 0; i < z_info->pack_size; i++) {
 		if (p->upkeep->inven[i] == obj) {
-			return I2A(i);
+			return labels[i];
 		}
 	}
 
@@ -898,7 +901,7 @@ void inven_wield(struct object *obj, int slot)
 				player);
 
 	/* Message */
-	msgt(MSG_WIELD, fmt, o_name, I2A(slot));
+	msgt(MSG_WIELD, fmt, o_name, gear_to_label(player, wielded));
 
 	/* Sticky flag geats a special mention */
 	if (of_has(wielded->flags, OF_CURSED)) {

src/ui-birth.c
@@ -458,9 +458,10 @@ static void init_birth_menu(struct menu *menu, int n_choices,
 	/* Initialise a basic menu */
 	menu_init(menu, MN_SKIN_SCROLL, &birth_iter);
 
-	/* A couple of behavioural flags - we want selections letters in
-	   lower case and a double tap to act as a selection. */
-	menu->selections = lower_case;
+	/* A couple of behavioural flags - we want selections as letters
+	   skipping the rogue-like cardinal direction movements and a
+	   double tap to act as a selection. */
+	menu->selections = all_letters_nohjkl;
 	menu->flags = MN_DBL_TAP;
 
 	/* Copy across the game's suggested initial selection, etc. */

src/ui-effect.c
@@ -40,7 +40,7 @@ static struct menu *effect_menu_new(struct effect *effect, int count,
 	char **ms;
 	char buf[80];
 
-	m->selections = lower_case;
+	m->selections = all_letters_nohjkl;
 
 	/* Collect a string for each effect. */
 	if (count > 0) {

src/ui-history.c
@@ -96,12 +96,14 @@ void history_display(void)
 				break;
 			}
 
+			case 'j':
 			case ARROW_DOWN: {
 				size_t scroll_to = first_item + 1;
 				first_item = (scroll_to < max_item ? scroll_to : max_item);
 				break;
 			}
 
+			case 'k':
 			case ARROW_UP: {
 				int scroll_to = first_item - 1;
 				first_item = (scroll_to >= 0 ? scroll_to : 0);

src/ui-knowledge.c
@@ -769,16 +769,11 @@ static void display_knowledge(const char *title, int *obj_list, int o_count,
 	int wid, hgt;
 	int i;
 	int prev_g = -1;
-
-	int omode = OPT(player, angband_keyset);
 	ui_event ke;
 
 	/* Get size */
 	Term_get_size(&wid, &hgt);
 	browser_rows = hgt - 8;
-
-	/* Disable the roguelike commands for the duration */
-	OPT(player, angband_keyset) = false;
 
 	/* Determine if using tiles or not */
 	if (tiles) tiles = (current_graphics_mode->grafID != 0);
@@ -1056,9 +1051,6 @@ static void display_knowledge(const char *title, int *obj_list, int o_count,
 			redraw = true;
 		}
 	}
-
-	/* Restore roguelike option */
-	OPT(player, angband_keyset) = omode;
 
 	/* Prompt */
 	if (!grp_cnt)
@@ -2072,10 +2064,10 @@ static const char *feat_prompt(int oid)
 {
 	(void)oid;
 		switch (f_uik_lighting) {
-				case LIGHTING_LIT:  return ", 'l/L' for lighting (lit)";
-                case LIGHTING_TORCH: return ", 'l/L' for lighting (torch)";
-				case LIGHTING_LOS:  return ", 'l/L' for lighting (LOS)";
-				default:	return ", 'l/L' for lighting (dark)";
+				case LIGHTING_LIT:  return ", 't/T' for lighting (lit)";
+                case LIGHTING_TORCH: return ", 't/T' for lighting (torch)";
+				case LIGHTING_LOS:  return ", 't/T' for lighting (LOS)";
+				default:	return ", 't/T' for lighting (dark)";
 		}		
 }
 
@@ -2085,14 +2077,14 @@ static const char *feat_prompt(int oid)
 static void f_xtra_act(struct keypress ch, int oid)
 {
 	/* XXX must be a better way to cycle this */
-	if (ch.code == 'l') {
+	if (ch.code == 't') {
 		switch (f_uik_lighting) {
 				case LIGHTING_LIT:  f_uik_lighting = LIGHTING_TORCH; break;
                 case LIGHTING_TORCH: f_uik_lighting = LIGHTING_LOS; break;
 				case LIGHTING_LOS:  f_uik_lighting = LIGHTING_DARK; break;
 				default:	f_uik_lighting = LIGHTING_LIT; break;
 		}		
-	} else if (ch.code == 'L') {
+	} else if (ch.code == 'T') {
 		switch (f_uik_lighting) {
 				case LIGHTING_DARK:  f_uik_lighting = LIGHTING_LOS; break;
                 case LIGHTING_LOS: f_uik_lighting = LIGHTING_TORCH; break;
@@ -2257,7 +2249,7 @@ static void trap_lore(int oid)
 static const char *trap_prompt(int oid)
 {
 	(void)oid;
-	return ", 'l' to cycle lighting";
+	return ", 't' to cycle lighting";
 }
 
 /**
@@ -2266,14 +2258,14 @@ static const char *trap_prompt(int oid)
 static void t_xtra_act(struct keypress ch, int oid)
 {
 	/* XXX must be a better way to cycle this */
-	if (ch.code == 'l') {
+	if (ch.code == 't') {
 		switch (t_uik_lighting) {
 				case LIGHTING_LIT:  t_uik_lighting = LIGHTING_TORCH; break;
                 case LIGHTING_TORCH: t_uik_lighting = LIGHTING_LOS; break;
 				case LIGHTING_LOS:  t_uik_lighting = LIGHTING_DARK; break;
 				default:	t_uik_lighting = LIGHTING_LIT; break;
 		}		
-	} else if (ch.code == 'L') {
+	} else if (ch.code == 'T') {
 		switch (t_uik_lighting) {
 				case LIGHTING_DARK:  t_uik_lighting = LIGHTING_LOS; break;
                 case LIGHTING_LOS: t_uik_lighting = LIGHTING_TORCH; break;
@@ -2355,7 +2347,7 @@ void textui_knowledge_init(void)
 	menu_setpriv(menu, N_ELEMENTS(knowledge_actions), knowledge_actions);
 
 	menu->title = "Display current knowledge";
-	menu->selections = lower_case;
+	menu->selections = all_letters_nohjkl;
 	/* Shortcuts to get the contents of the stores by number; does prevent
 	 * the normal use of 4 and 6 to go to the previous or next menu */
 	menu->cmd_keys = "12345678";
@@ -2573,21 +2565,25 @@ void do_cmd_messages(void)
 
 				case ARROW_LEFT:
 				case '4':
+				case 'h':
 					q = (q >= wid / 2) ? (q - wid / 2) : 0;
 					break;
 
 				case ARROW_RIGHT:
 				case '6':
+				case 'l':
 					q = q + wid / 2;
 					break;
 
 				case ARROW_UP:
 				case '8':
+				case 'k':
 					if (i + 1 < n) i += 1;
 					break;
 
 				case ARROW_DOWN:
 				case '2':
+				case 'j':
 				case KC_ENTER:
 					i = (i >= 1) ? (i - 1) : 0;
 					break;

src/ui-menu.c
@@ -39,6 +39,7 @@ const uint8_t curs_attrs[3][2] =
 const char lower_case[] = "abcdefghijklmnopqrstuvwxyz";
 const char upper_case[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const char all_letters[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const char all_letters_nohjkl[] = "abcdefgimnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 /**
  * Forward declarations

src/ui-menu.h
@@ -58,6 +58,7 @@ extern const uint8_t curs_attrs[3][2];
 extern const char lower_case[];			/* abc..z */
 extern const char upper_case[];			/* ABC..Z */
 extern const char all_letters[];		/* abc..zABC..Z */
+extern const char all_letters_nohjkl[];		/* abc..gim..zABC..Z */
 
 
 /*

src/ui-object.c
@@ -242,7 +242,7 @@ static void build_obj_list(int last, struct object **list, item_tester tester,
 		/* Acceptable items get a label */
 		if (object_test(tester, obj))
 			strnfmt(items[num_obj].label, sizeof(items[num_obj].label), "%c) ",
-					I2A(i));
+				all_letters_nohjkl[i]);
 
 		/* Unacceptable items are still sometimes shown */
 		else if ((!obj && show_empty) || in_term)
@@ -623,7 +623,8 @@ static void menu_header(void)
 		/* List choices */
 		if (i1 <= i2) {
 			/* Build the header */
-			strnfmt(tmp_val, sizeof(tmp_val), " %c-%c,", I2A(i1), I2A(i2));
+			strnfmt(tmp_val, sizeof(tmp_val), " %c-%c,",
+				all_letters_nohjkl[i1], all_letters_nohjkl[i2]);
 
 			/* Append */
 			my_strcat(out_val, tmp_val, sizeof(out_val));
@@ -646,7 +647,8 @@ static void menu_header(void)
 		/* List choices */
 		if (e1 <= e2) {
 			/* Build the header */
-			strnfmt(tmp_val, sizeof(tmp_val), " %c-%c,", I2A(e1), I2A(e2));
+			strnfmt(tmp_val, sizeof(tmp_val), " %c-%c,",
+				all_letters_nohjkl[e1], all_letters_nohjkl[e2]);
 
 			/* Append */
 			my_strcat(out_val, tmp_val, sizeof(out_val));
@@ -669,7 +671,8 @@ static void menu_header(void)
 		/* List choices */
 		if (throwing_num) {
 			/* Build the header */
-			strnfmt(tmp_val, sizeof(tmp_val),  " a-%c,", I2A(throwing_num - 1));
+			strnfmt(tmp_val, sizeof(tmp_val),  " a-%c,",
+				all_letters_nohjkl[throwing_num - 1]);
 
 			/* Append */
 			my_strcat(out_val, tmp_val, sizeof(out_val));
@@ -692,7 +695,8 @@ static void menu_header(void)
 		/* List choices */
 		if (f1 <= f2) {
 			/* Build the header */
-			strnfmt(tmp_val, sizeof(tmp_val), " %c-%c,", I2A(f1), I2A(f2));
+			strnfmt(tmp_val, sizeof(tmp_val), " %c-%c,",
+				all_letters_nohjkl[f1], all_letters_nohjkl[f2]);
 
 			/* Append */
 			my_strcat(out_val, tmp_val, sizeof(out_val));
@@ -810,7 +814,7 @@ static struct object *item_menu(cmd_code cmd, int prompt_size, int mode)
 
 	/* Set up the menu */
 	menu_setpriv(m, num_obj, items);
-	m->selections = lower_case;
+	m->selections = all_letters_nohjkl;
 	m->switch_keys = "/|-";
 	m->flags = (MN_PVT_TAGS | MN_INSCRIP_TAGS);
 

src/ui-options.c
@@ -304,6 +304,7 @@ static const menu_iter option_toggle_iter = {
  */
 static void option_toggle_menu(const char *name, int page)
 {
+	static const char selections[] = "abcdefgimopquvwzABCDEFGHIJKLMOPQUVWZ";
 	int i;
 	
 	struct menu *m = menu_new(MN_SKIN_SCROLL, &option_toggle_iter);
@@ -311,7 +312,7 @@ static void option_toggle_menu(const char *name, int page)
 	/* for all menus */
 	m->prompt = "Set option (y/n/t), select with movement keys or index";
 	m->cmd_keys = "YyNnTt";
-	m->selections = "abcdefghijklmopqrsuvwxz";
+	m->selections = selections;
 	m->flags = MN_DBL_TAP;
 
 	/* We add 10 onto the page amount to indicate we're at birth */
@@ -322,7 +323,6 @@ static void option_toggle_menu(const char *name, int page)
 	} else if (page == OPT_PAGE_BIRTH + 10 || page == OP_INTERFACE) {
 		m->prompt = "Set option (y/n/t), 's' to save, 'r' to restore, 'x' to reset";
 		m->cmd_keys = "YyNnTtSsRrXx";
-		m->selections = "abcdefghijklmopquvwzABC";
 		/* Provide a context menu for equivalents to 's', 'r', .... */
 		m->context_hook = use_option_context_menu;
 		if (page == OPT_PAGE_BIRTH + 10) {
@@ -1858,7 +1858,7 @@ static char tag_options_item(struct menu *menu, int oid)
 	size_t line = (size_t) oid;
 
 	if (line < N_ELEMENTS(sval_dependent))
-		return I2A(oid);
+		return all_letters_nohjkl[oid];
 
 	/* Separator - blank line. */
 	if (line == N_ELEMENTS(sval_dependent))
@@ -1991,8 +1991,8 @@ static menu_action option_actions[] =
 	{ 0, 's', "Save subwindow setup to pref file", do_dump_options },
 	{ 0, 't', "Save autoinscriptions to pref file", do_dump_autoinsc },
 	{ 0, 0, NULL, NULL },
-	{ 0, 'l', "Load a user pref file", options_load_pref_file },
-	{ 0, 'k', "Edit keymaps (advanced)", do_cmd_keymaps },
+	{ 0, 'p', "Load a user pref file", options_load_pref_file },
+	{ 0, 'e', "Edit keymaps (advanced)", do_cmd_keymaps },
 	{ 0, 'c', "Edit colours (advanced)", do_cmd_colors },
 	{ 0, 'v', "Save visuals (advanced)", do_cmd_visuals },
 };

src/ui-spoil.c
@@ -61,7 +61,7 @@ void do_cmd_spoilers(void)
 	if (!spoil_menu) {
 		spoil_menu = menu_new_action(spoil_actions,
 			N_ELEMENTS(spoil_actions));
-		spoil_menu->selections = lower_case;
+		spoil_menu->selections = all_letters_nohjkl;
 		spoil_menu->title = "Create spoilers";
 	}
 