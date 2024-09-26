angband/angband

Avoid mismatched format string warnings on platforms (Nintendo and DOS) where int is 16-bit.

src/score.c
@@ -231,10 +231,10 @@ void build_score(struct high_score *entry, const struct player *p,
 	strnfmt(entry->pts, sizeof(entry->pts), "%9ld", total_points(p));
 
 	/* Save the current gold */
-	strnfmt(entry->gold, sizeof(entry->gold), "%9u", p->au);
+	strnfmt(entry->gold, sizeof(entry->gold), "%9ld", (long)p->au);
 
 	/* Save the current turn */
-	strnfmt(entry->turns, sizeof(entry->turns), "%9u", turn);
+	strnfmt(entry->turns, sizeof(entry->turns), "%9ld", (long)turn);
 
 	/* Time of death */
 	if (death_time)

NickMcConnell/Beleriand

src/score.c
@@ -263,7 +263,7 @@ void build_score(struct high_score *entry, const struct player *p,
 	strnfmt(entry->what, sizeof(entry->what), "%s", buildid);
 
 	/* Save the current turn */
-	strnfmt(entry->turns, sizeof(entry->turns), "%9u", p->turn);
+	strnfmt(entry->turns, sizeof(entry->turns), "%9ld", (long)p->turn);
 
 	/* Time of death */
 	if (death_time)