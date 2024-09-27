f793479681d1d82ee723b62a0c4e8297bc3ca352
Eric Branlund <ebranlund@fastmail.com>
2024-09-27 12:33:52 -0600

    Port first batch of additional init.c tests from Vanilla Angband

--- src/tests/parse/f-info.c
+++ src/tests/parse/f-info.c
@@ -1,8 +1,13 @@
 /* parse/f-info */
+/* Exercise parsing used for terrain.txt. */
 
 #include "unit-test.h"
 #include "unit-test-data.h"
+#include "cave.h"
 #include "init.h"
+#include "monster.h"
+#include <locale.h>
+#include <langinfo.h>
 
 
 int setup_tests(void **state) {
@@ -11,7 +16,15 @@ int setup_tests(void **state) {
 }
 
 int teardown_tests(void *state) {
-	struct feature *f = parser_priv(state);
+	struct parser *p = (struct parser*) state;
+	struct feature *f = (struct feature*) parser_priv(p);
+
+	string_free(f->look_in_preposition);
+	string_free(f->look_prefix);
+	string_free(f->confused_msg);
+	string_free(f->str_msg);
+	string_free(f->fail_msg);
+	string_free(f->dig_msg);
 	string_free(f->die_msg);
 	string_free(f->hurt_msg);
 	string_free(f->run_msg);
@@ -20,32 +33,102 @@ int teardown_tests(void *state) {
 	string_free(f->desc);
 	string_free(f->name);
 	mem_free(f);
-	parser_destroy(state);
+	parser_destroy(p);
 	return 0;
 }
 
+static int test_missing_header_record0(void *state) {
+	struct parser *p = (struct parser*) state;
+	struct feature *f = (struct feature*) parser_priv(p);
+	enum parser_error r;
+
+	null(f);
+	r = parser_parse(p, "graphics: :w");
+	eq(r, PARSE_ERROR_MISSING_RECORD_HEADER);
+	r = parser_parse(p, "priority:2");
+	eq(r, PARSE_ERROR_MISSING_RECORD_HEADER);
+	r = parser_parse(p, "mimic:granite wall");
+	eq(r, PARSE_ERROR_MISSING_RECORD_HEADER);
+	r = parser_parse(p, "flags:LOS | PASSABLE");
+	eq(r, PARSE_ERROR_MISSING_RECORD_HEADER);
+	r = parser_parse(p, "info:0:3:0");
+	eq(r, PARSE_ERROR_MISSING_RECORD_HEADER);
+	ok;
+}
+
 static int test_name0(void *state) {
-	enum parser_error r = parser_parse(state, "name:Test Feature");
+	struct parser *p = (struct parser*) state;
+	enum parser_error r = parser_parse(p, "name:Test Feature");
 	struct feature *f;
 
 	eq(r, PARSE_ERROR_NONE);
-	f = parser_priv(state);
-	require(f);
+	f = (struct feature*) parser_priv(p);
+	notnull(f);
 	require(streq(f->name, "Test Feature"));
+	null(f->desc);
+	null(f->mimic);
+	eq(f->priority, 0);
+	eq(f->dig, 0);
+	eq(f->pit_difficulty, 0);
+	require(flag_is_empty(f->flags, TF_SIZE));
+	eq(f->d_attr, 0);
+	eq(f->d_char, 0);
+	null(f->walk_msg);
+	null(f->run_msg);
+	null(f->hurt_msg);
+	null(f->die_msg);
+	null(f->dig_msg);
+	null(f->fail_msg);
+	null(f->str_msg);
+	null(f->confused_msg);
+	null(f->look_prefix);
+	null(f->look_in_preposition);
 	ok;
 }
 
 static int test_graphics0(void *state) {
-	enum parser_error r = parser_parse(state, "graphics:::red");
+	struct parser *p = (struct parser*) state;
+	enum parser_error r = parser_parse(p, "graphics:::Light Green");
 	struct feature *f;
 
 	eq(r, PARSE_ERROR_NONE);
-	f = parser_priv(state);
-	require(f);
+	f = (struct feature*) parser_priv(p);
+	notnull(f);
 	eq(f->d_char, L':');
-	eq(f->d_attr, COLOUR_RED);
+	eq(f->d_attr, COLOUR_L_GREEN);
+	/* Check that single letter code for color works. */
+	r = parser_parse(p, "graphics:^:b");
+	eq(r, PARSE_ERROR_NONE);
+	f = (struct feature*) parser_priv(p);
+	notnull(f);
+	eq(f->d_char, L'^');
+	eq(f->d_attr, COLOUR_BLUE);
+	/* Check that full name matching for color is case insensitive. */
+	r = parser_parse(p, "graphics:#:light purple");
+	f = (struct feature*) parser_priv(p);
+	notnull(f);
+	eq(f->d_char, L'#');
+	eq(f->d_attr, COLOUR_L_PURPLE);
+	if (setlocale(LC_CTYPE, "") && streq(nl_langinfo(CODESET), "UTF-8")) {
+		/*
+		 * Check for glyph that is outside of the ASCII range.  Use
+		 * the Yen sign, Unicode U+00A5 or C2 A5 as UTF-8.
+		 */
+		wchar_t wcs[3];
+		size_t nc;
+
+		r = parser_parse(p, "graphics:¥:red");
+		eq(r, PARSE_ERROR_NONE);
+		nc = text_mbstowcs(wcs, "¥", (int) N_ELEMENTS(wcs));
+		eq(nc, 1);
+		f = (struct feature*) parser_priv(p);
+		notnull(f);
+		eq(f->d_char, wcs[0]);
+		eq(f->d_attr, COLOUR_RED);
+	}
 	ok;
 }
+
 
 static int test_mimic0(void *state) {
 	enum parser_error r = parser_parse(state, "mimic:marshmallow");
@@ -70,13 +153,32 @@ static int test_priority0(void *state) {
 }
 
 static int test_flags0(void *state) {
-	enum parser_error r = parser_parse(state, "flags:LOS | PERMANENT | DOWNSTAIR");
-	struct feature *f;
+	struct parser *p = (struct parser*) state;
+	struct feature *f = (struct feature*) parser_priv(p);
+	enum parser_error r;
+	bitflag eflags[TF_SIZE];
 
+	notnull(f);
+	flag_wipe(f->flags, TF_SIZE);
+	/* Check that specifying no flags works. */
+	r = parser_parse(p, "flags:");
 	eq(r, PARSE_ERROR_NONE);
-	f = parser_priv(state);
-	require(f);
-	require(f->flags);
+	f = (struct feature*) parser_priv(p);
+	notnull(f);
+	require(flag_is_empty(f->flags, TF_SIZE));
+	/* Try setting one flag. */
+	r = parser_parse(p, "flags:LOS");
+	eq(r, PARSE_ERROR_NONE);
+	/* Try setting more than one flag. */
+	r = parser_parse(p, "flags:PERMANENT | DOWNSTAIR");
+	eq(r, PARSE_ERROR_NONE);
+	f = (struct feature*) parser_priv(p);
+	notnull(f);
+	flag_wipe(eflags, TF_SIZE);
+	flag_on_dbg(eflags, TF_SIZE, TF_LOS, "eflags", "TF_LOS");
+	flag_on_dbg(eflags, TF_SIZE, TF_PERMANENT, "eflags", "TF_PERMANENT");
+	flag_on_dbg(eflags, TF_SIZE, TF_DOWNSTAIR, "eflags", "TF_DOWNSTAIR");
+	require(flag_is_equal(f->flags, eflags, TF_SIZE));
 	ok;
 }
 
@@ -93,102 +195,202 @@ static int test_info0(void *state) {
 	ok;
 }
 
-static int test_walk_msg0(void *state) {
-	enum parser_error r = parser_parse(state, "walk-msg:lookout ");
+static int test_desc0(void *state) {
+	struct parser *p = (struct parser*) state;
+	enum parser_error r = parser_parse(p,
+		"desc:A door that is already open.");
 	struct feature *f;
 
 	eq(r, PARSE_ERROR_NONE);
-	f = parser_priv(state);
-	require(f);
-	require(streq(f->walk_msg, "lookout "));
+	/* Check that additional directives are appended to the first. */
+	r = parser_parse(p, "desc:  Player, monster, spell, and missile "
+		"can pass through as long as it stays open.");
+	eq(r, PARSE_ERROR_NONE);
+	f = (struct feature*) parser_priv(p);
+	notnull(f);
+	notnull(f->desc);
+	require(streq(f->desc, "A door that is already open.  Player, "
+		"monster, spell, and missile can pass through as long as it "
+		"stays open."));
+	ok;
+}
+
+static int test_walk_msg0(void *state) {
+	struct parser *p = (struct parser*) state;
+	enum parser_error r = parser_parse(p, "walk-msg:It looks dangerous.");
+	struct feature *f;
+
+	eq(r, PARSE_ERROR_NONE);
+	/* Check that additional directives are appended to the first. */
+	r = parser_parse(p, "walk-msg:  Really enter? ");
+	eq(r, PARSE_ERROR_NONE);
+	f = (struct feature*) parser_priv(p);
+	notnull(f);
+	notnull(f->walk_msg);
+	require(streq(f->walk_msg, "It looks dangerous.  Really enter? "));
 	ok;
 }
 
 static int test_run_msg0(void *state) {
-	enum parser_error r = parser_parse(state, "run-msg:lookout! ");
+	struct parser *p = (struct parser*) state;
+	enum parser_error r = parser_parse(p, "run-msg:It blocks your path.");
 	struct feature *f;
 
 	eq(r, PARSE_ERROR_NONE);
-	f = parser_priv(state);
-	require(f);
-	require(streq(f->run_msg, "lookout! "));
+	/* Check that additional directives are appended to the first. */
+	r = parser_parse(p, "run-msg:  Really enter? ");
+	eq(r, PARSE_ERROR_NONE);
+	f = (struct feature*) parser_priv(p);
+	notnull(f);
+	notnull(f->run_msg);
+	require(streq(f->run_msg, "It blocks your path.  Really enter? "));
 	ok;
 }
 
 static int test_hurt_msg0(void *state) {
-	enum parser_error r = parser_parse(state, "hurt-msg:ow!");
+	struct parser *p = (struct parser*) state;
+	enum parser_error r = parser_parse(p, "hurt-msg:Ow!");
 	struct feature *f;
 
 	eq(r, PARSE_ERROR_NONE);
-	f = parser_priv(state);
-	require(f);
-	require(streq(f->hurt_msg, "ow!"));
+	/* Check that additional directives are appended to the first. */
+	r = parser_parse(p, "hurt-msg:  That hurt!");
+	eq(r, PARSE_ERROR_NONE);
+	f = (struct feature*) parser_priv(p);
+	notnull(f);
+	notnull(f->hurt_msg);
+	require(streq(f->hurt_msg, "Ow!  That hurt!"));
 	ok;
 }
 
 static int test_die_msg0(void *state) {
-	enum parser_error r = parser_parse(state, "die-msg:aargh");
+	struct parser *p = (struct parser*) state;
+	enum parser_error r = parser_parse(p, "die-msg:dissolving");
 	struct feature *f;
 
 	eq(r, PARSE_ERROR_NONE);
-	f = parser_priv(state);
-	require(f);
-	require(streq(f->die_msg, "aargh"));
+	/* Check that additional directives are appended to the first. */
+	r = parser_parse(p, "die-msg: in a pool of acid");
+	eq(r, PARSE_ERROR_NONE);
+	f = (struct feature*) parser_priv(p);
+	notnull(f);
+	notnull(f->die_msg);
+	require(streq(f->die_msg, "dissolving in a pool of acid"));
 	ok;
 }
 
 static int test_dig_msg0(void *state) {
-	enum parser_error r = parser_parse(state, "dig-msg:urgh");
+	struct parser *p = (struct parser*) state;
+	enum parser_error r = parser_parse(p, "dig-msg:You clear the rubble.");
 	struct feature *f;
 
 	eq(r, PARSE_ERROR_NONE);
-	f = parser_priv(state);
-	require(f);
-	require(streq(f->dig_msg, "urgh"));
+	/* Check that additional directives are appended to the first. */
+	r = parser_parse(p, "dig-msg:  Revealing a dusty floor.");
+	eq(r, PARSE_ERROR_NONE);
+	f = (struct feature*) parser_priv(p);
+	notnull(f);
+	notnull(f->dig_msg);
+	require(streq(f->dig_msg, "You clear the rubble.  Revealing a dusty floor."));
 	ok;
 }
 
 static int test_fail_msg0(void *state) {
-	enum parser_error r = parser_parse(state, "fail-msg:D'oh");
+	struct parser *p = (struct parser*) state;
+	enum parser_error r = parser_parse(p,
+		"fail-msg:You are unable to shift the rubble with your {name}.");
 	struct feature *f;
 
 	eq(r, PARSE_ERROR_NONE);
-	f = parser_priv(state);
-	require(f);
-	require(streq(f->fail_msg, "D'oh"));
+	/* Check that additional directives are appended to the first. */
+	r = parser_parse(p, "fail-msg:  And develop a blister on your palm.");
+	eq(r, PARSE_ERROR_NONE);
+	f = (struct feature*) parser_priv(p);
+	notnull(f);
+	notnull(f->fail_msg);
+	require(streq(f->fail_msg, "You are unable to shift the rubble with "
+		"your {name}.  And develop a blister on your palm."));
 	ok;
 }
 
 static int test_str_msg0(void *state) {
-	enum parser_error r = parser_parse(state, "str-msg:hnnnh");
+	struct parser *p = (struct parser*) state;
+	enum parser_error r = parser_parse(p,
+		"str-msg:You are not strong enough to shift the rubble.");
 	struct feature *f;
 
 	eq(r, PARSE_ERROR_NONE);
-	f = parser_priv(state);
-	require(f);
-	require(streq(f->str_msg, "hnnnh"));
+	/* Check that additional directives are appended to the first. */
+	r = parser_parse(p, "str-msg:  Eat more spinach next time.");
+	eq(r, PARSE_ERROR_NONE);
+	f = (struct feature*) parser_priv(p);
+	notnull(f);
+	notnull(f->str_msg);
+	require(streq(f->str_msg,
+		"You are not strong enough to shift the rubble.  Eat more "
+		"spinach next time."));
 	ok;
 }
 
 static int test_confused_msg0(void *state) {
-	enum parser_error r = parser_parse(state, "confused-msg:huh?");
+	struct parser *p = (struct parser*) state;
+	enum parser_error r = parser_parse(p, "confused-msg:slams into a wall");
 	struct feature *f;
 
 	eq(r, PARSE_ERROR_NONE);
-	f = parser_priv(state);
-	require(f);
-	require(streq(f->confused_msg, "huh?"));
+	/* Check that additional directives are appended to the first. */
+	r = parser_parse(p, "confused-msg: and stumbles");
+	eq(r, PARSE_ERROR_NONE);
+	f = (struct feature*) parser_priv(p);
+	notnull(f);
+	notnull(f->confused_msg);
+	require(streq(f->confused_msg, "slams into a wall and stumbles"));
+	ok;
+}
+
+static int test_look_prefix0(void *state) {
+	struct parser *p = (struct parser*) state;
+	enum parser_error r = parser_parse(p, "look-prefix:the entrance ");
+	struct feature *f;
+
+	eq(r, PARSE_ERROR_NONE);
+	/* Check that additional directives are appended to the first. */
+	r = parser_parse(p, "look-prefix:to the");
+	eq(r, PARSE_ERROR_NONE);
+	f = (struct feature*) parser_priv(p);
+	notnull(f);
+	notnull(f->look_prefix);
+	require(streq(f->look_prefix, "the entrance to the"));
+	ok;
+}
+
+static int test_look_in_preposition0(void *state) {
+	struct parser *p = (struct parser*) state;
+	enum parser_error r = parser_parse(p, "look-in-preposition:at the ");
+	struct feature *f;
+
+	eq(r, PARSE_ERROR_NONE);
+	/* Check that additional directives are appended to the first. */
+	r = parser_parse(p, "look-in-preposition:brink of");
+	eq(r, PARSE_ERROR_NONE);
+	f = (struct feature*) parser_priv(p);
+	notnull(f);
+	notnull(f->look_in_preposition);
+	require(streq(f->look_in_preposition, "at the brink of"));
 	ok;
 }
 
 const char *suite_name = "parse/f-info";
+/* test_missing_header_record0() has to be before test_name0(). */
 struct test tests[] = {
+	{ "missing_header_record0", test_missing_header_record0 },
 	{ "name0", test_name0 },
 	{ "graphics0", test_graphics0 },
 	{ "mimic0", test_mimic0 },
 	{ "priority0", test_priority0 },
 	{ "flags0", test_flags0 },
 	{ "info0", test_info0 },
+	{ "desc0", test_desc0 },
 	{ "walk_msg0", test_walk_msg0 },
 	{ "run_msg0", test_run_msg0 },
 	{ "hurt_msg0", test_hurt_msg0 },
@@ -197,5 +399,7 @@ struct test tests[] = {
 	{ "fail_msg0", test_fail_msg0 },
 	{ "str_msg0", test_str_msg0 },
 	{ "confused_msg0", test_confused_msg0 },
+	{ "look_prefix0", test_look_prefix0 },
+	{ "look_in_preposition0", test_look_in_preposition0 },
 	{ NULL, NULL }
 };
--- src/tests/parse/p-info.c
+++ src/tests/parse/p-info.c
@@ -1,7 +1,9 @@
 /* parse/p-info */
+/* Exercise parsing used for race.txt. */
 
 #include "unit-test.h"
 #include "init.h"
+#include "object.h"
 #include "player.h"
 
 
@@ -12,10 +14,46 @@ int setup_tests(void **state) {
 
 int teardown_tests(void *state) {
 	struct player_race *pr = parser_priv(state);
+	struct start_item *si = pr->start_items;
+
+	while (si) {
+		struct start_item *nsi = si->next;
+
+		mem_free(si);
+		si = nsi;
+	}
+	string_free((char *)pr->desc);
 	string_free((char *)pr->name);
 	mem_free(pr);
 	parser_destroy(state);
 	return 0;
+}
+
+static int test_missing_record_header0(void *state) {
+	struct parser *p = (struct parser*) state;
+	struct player_race *pr = (struct player_race*) parser_priv(p);
+	enum parser_error r;
+
+	null(pr);
+	r = parser_parse(p, "stats:0:1:2:2");
+	eq(r, PARSE_ERROR_MISSING_RECORD_HEADER);
+	r = parser_parse(p, "skills:0:1:0:0:0:0:0:0");
+	eq(r, PARSE_ERROR_MISSING_RECORD_HEADER);
+	r = parser_parse(p, "history:1");
+	eq(r, PARSE_ERROR_MISSING_RECORD_HEADER);
+	r = parser_parse(p, "age:20:4865");
+	eq(r, PARSE_ERROR_MISSING_RECORD_HEADER);
+	r = parser_parse(p, "height:76:3");
+	eq(r, PARSE_ERROR_MISSING_RECORD_HEADER);
+	r = parser_parse(p, "weight:159:10");
+	eq(r, PARSE_ERROR_MISSING_RECORD_HEADER);
+	r = parser_parse(p, "player-flags:BLADE_PROFICIENCY");
+	eq(r, PARSE_ERROR_MISSING_RECORD_HEADER);
+	r = parser_parse(p, "equip:food:Fragment of Lembas:3:3");
+	eq(r, PARSE_ERROR_MISSING_RECORD_HEADER);
+	r = parser_parse(p, "desc:The dwarves are stone-hard and stubborn, ");
+	eq(r, PARSE_ERROR_MISSING_RECORD_HEADER);
+	ok;
 }
 
 static int test_name0(void *state) {
@@ -109,13 +147,53 @@ static int test_weight0(void *state) {
 }
 
 static int test_play_flags0(void *state) {
-	enum parser_error r = parser_parse(state, "player-flags:BLADE_PROFICIENCY");
-	struct player_race *pr;
+	struct parser *p = (struct parser*) state;
+	struct player_race *pr = (struct player_race*) parser_priv(p);
+	enum parser_error r;
+	bitflag eflags[PF_SIZE];
 
+	notnull(pr);
+	pf_wipe(pr->pflags);
+	/* Check that specifying no flags works. */
+	r = parser_parse(p, "player-flags:");
 	eq(r, PARSE_ERROR_NONE);
-	pr = parser_priv(state);
-	require(pr);
-	require(pr->pflags);
+	pr = (struct player_race*) parser_priv(p);
+	notnull(pr);
+	require(pf_is_empty(pr->pflags));
+	/* Try one flag. */
+	r = parser_parse(p, "player-flags:BLADE_PROFICIENCY");
+	eq(r, PARSE_ERROR_NONE);
+	pf_wipe(eflags);
+	pf_on(eflags, PF_BLADE_PROFICIENCY);
+	pr = (struct player_race*) parser_priv(p);
+	notnull(pr);
+	require(pf_is_equal(pr->pflags, eflags));
+	/* Check that multiple player-flags lines concatenate the flags. */
+	r = parser_parse(p, "player-flags:AXE_PROFICIENCY");
+	eq(r, PARSE_ERROR_NONE);
+	pf_on(eflags, PF_AXE_PROFICIENCY);
+	pr = (struct player_race*) parser_priv(p);
+	notnull(pr);
+	require(pf_is_equal(pr->pflags, eflags));
+	/* Try multiple flags at once. */
+	pf_wipe(pr->pflags);
+	r = parser_parse(p, "player-flags:AXE_PROFICIENCY | BLADE_PROFICIENCY");
+	eq(r, PARSE_ERROR_NONE);
+	pf_wipe(eflags);
+	pf_on(eflags, PF_AXE_PROFICIENCY);
+	pf_on(eflags, PF_BLADE_PROFICIENCY);
+	pr = (struct player_race*) parser_priv(p);
+	notnull(pr);
+	require(pf_is_equal(pr->pflags, eflags));
+	ok;
+}
+
+static int test_play_flags_bad0(void *state) {
+	struct parser *p = (struct parser*) state;
+	/* Try an unrecognized flag. */
+	enum parser_error r = parser_parse(p, "player-flags:XYZZY");
+
+	eq(r, PARSE_ERROR_INVALID_FLAG);
 	ok;
 }
 
@@ -133,8 +211,48 @@ static int test_equip0(void *state) {
 	ok;
 }
 
+static int test_equip_bad0(void *state) {
+	struct parser *p = (struct parser*) state;
+	/* Try an unrecognized tval name. */
+	enum parser_error r = parser_parse(p, "equip:xyzzy:1:1:1");
+
+	eq(r, PARSE_ERROR_UNRECOGNISED_TVAL);
+	/* Try invalid minimums or maximums. */
+	r = parser_parse(p, "equip:sword:1:1:105");
+	eq(r, PARSE_ERROR_INVALID_ITEM_NUMBER);
+	r = parser_parse(p, "equip:sword:1:120:1");
+	eq(r, PARSE_ERROR_INVALID_ITEM_NUMBER);
+	r = parser_parse(p, "equip:sword:1:700:800");
+	eq(r, PARSE_ERROR_INVALID_ITEM_NUMBER);
+
+	ok;
+}
+
+static int test_desc0(void *state) {
+	struct parser *p = (struct parser*) state;
+	enum parser_error r = parser_parse(p,
+		"desc: The dwarves are stone-hard and stubborn, ");
+	struct player_race *pr;
+
+	eq(r, PARSE_ERROR_NONE);
+	/* Check that multiple directives are appended. */
+	r = parser_parse(p, "desc:fast in friendship and in enmity.");
+	eq(r, PARSE_ERROR_NONE);
+	pr = (struct player_race*) parser_priv(p);
+	notnull(pr);
+	notnull(pr->desc);
+	require(streq(pr->desc, "The dwarves are stone-hard and stubborn, "
+		"fast in friendship and in enmity."));
+	ok;
+}
+
 const char *suite_name = "parse/p-info";
+/*
+ * test_missing_record_header0() has to be before test_name0().  All others,
+ * except test_name0(), have to be after test_name0().
+ */
 struct test tests[] = {
+	{ "missing_record_header0", test_missing_record_header0 },
 	{ "name0", test_name0 },
 	{ "stats0", test_stats0 },
 	{ "skills0", test_skills0 },
@@ -143,6 +261,9 @@ struct test tests[] = {
 	{ "height0", test_height0 },
 	{ "weight0", test_weight0 },
 	{ "player_flags0", test_play_flags0 },
+	{ "player_flags_bad0", test_play_flags_bad0 },
 	{ "equip0", test_equip0 },
+	{ "equip_bad0", test_equip_bad0 },
+	{ "desc0", test_desc0 },
 	{ NULL, NULL }
 };
