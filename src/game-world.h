0c0cde720d808bc1c15a00919dbef89eba962621
Nick McConnell <nckmccnnll@gmail.com>
2026-07-09 06:54:12 +1000

    Assign colours to biomes for regional map

--- src/game-world.h
+++ src/game-world.h
@@ -81,7 +81,7 @@ enum {
  * Codes for the different surface biomes
  */
 enum biome_type {
-	#define BIOME(a, b) BIOME_##a = b,
+#define BIOME(a, b, c) BIOME_##a = c,
 	#include "list-biomes.h"
 	#undef BIOME
 };
--- src/list-biomes.h
+++ src/list-biomes.h
@@ -2,17 +2,17 @@
  * \file src/list-biomes.h
  * \brief biomes
  */
-BIOME(ALL,		0x21)		/**< ! */
-BIOME(CAVE,		0x24)		/**< $ */
-BIOME(SNOW,		0x2a)		/**< * */
-BIOME(FOREST,	0x2b)		/**< + */
-BIOME(MOOR,		0x2c)		/**< , */
-BIOME(LAKE,		0x2d)		/**< - */
-BIOME(PLAIN,	0x2e)		/**< . */
-BIOME(DESERT,	0x2f)		/**< / */
-BIOME(TOWN,		0x3d)		/**< = */
-BIOME(IMPASS,	0x58)		/**< X */
-BIOME(MOUNTAIN,	0x5e)		/**< ^ */
-BIOME(SWAMP,	0x5f)		/**< _ */
-BIOME(DARK,		0x7c)		/**< | */
-BIOME(OCEAN,	0x7e)		/**< ~ */
+BIOME(ALL,		COLOUR_VIOLET,	0x21)		/**< ! */
+BIOME(CAVE,		COLOUR_PURPLE,	0x24)		/**< $ */
+BIOME(SNOW,		COLOUR_WHITE,	0x2a)		/**< * */
+BIOME(FOREST,	COLOUR_GREEN,	0x2b)		/**< + */
+BIOME(MOOR,		COLOUR_MUSTARD,	0x2c)		/**< , */
+BIOME(LAKE,		COLOUR_TEAL,	0x2d)		/**< - */
+BIOME(PLAIN,	COLOUR_L_GREEN,	0x2e)		/**< . */
+BIOME(DESERT,	COLOUR_YELLOW,	0x2f)		/**< / */
+BIOME(TOWN,		COLOUR_L_WHITE,	0x3d)		/**< = */
+BIOME(IMPASS,	COLOUR_UMBER,	0x58)		/**< X */
+BIOME(MOUNTAIN,	COLOUR_L_UMBER,	0x5e)		/**< ^ */
+BIOME(SWAMP,	COLOUR_MUD,		0x5f)		/**< _ */
+BIOME(DARK,		COLOUR_SLATE,	0x7c)		/**< | */
+BIOME(OCEAN,	COLOUR_BLUE,	0x7e)		/**< ~ */
