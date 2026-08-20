163cb55e24249a6193fcb998582828b3193e0be1
Eric Branlund <ebranlund@fastmail.com>
2026-08-20 09:30:29 -0600

    Allow ^g to invoke autopickup
    Resolves https://github.com/angband/angband/issues/6688 .

--- src/ui-input.c
+++ src/ui-input.c
@@ -1943,7 +1943,7 @@ bool textui_process_key(struct keypress kp, unsigned char *c, int count)
 	keycode_t key = kp.code;
 
 	/* Null command */
-	if (key == '\0' || key == ESCAPE || key == ' ' || key == '\a')
+	if (key == '\0' || key == ESCAPE || key == ' ')
 		return true;
 
 	/* Invalid keypress */
